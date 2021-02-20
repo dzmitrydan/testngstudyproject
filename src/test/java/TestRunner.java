10b39b79ab675872976b87dbd2c40e2d1a7d499d
Dmitry Danilov <dmitry_danilov@epam.com>
2021-02-20 12:50:59 +0300

    refactoring task 1

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -2,31 +2,27 @@ import org.testng.TestNG;
 import org.testng.xml.XmlClass;
 import org.testng.xml.XmlSuite;
 import org.testng.xml.XmlTest;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TestRunner {
     public static void main(String[] args) {
-
         List<XmlSuite> suites = new ArrayList<>();
-
-        for (int i = 0; i < args.length; i++) {
-            XmlSuite suite = new XmlSuite();
-            suite.setName("TestSuite" + args[i]);
-
-            XmlTest test = new XmlTest(suite);
-            test.setName("Test" +  args[i]);
-
-            List<XmlClass> classes = new ArrayList<>();
-            classes.add(new XmlClass("test.Test" + args[i]));
-            test.setXmlClasses(classes) ;
-
-            suites.add(suite);
-        }
-
+        Arrays.stream(args).forEach(s -> suites.add(suiteBuilder(s)));
         TestNG tng = new TestNG();
         tng.setXmlSuites(suites);
         tng.run();
+    }
+
+    private static XmlSuite suiteBuilder(String testSuiteName) {
+        XmlSuite suite = new XmlSuite();
+        suite.setName("TestSuite" + testSuiteName);
+        XmlTest test = new XmlTest(suite);
+        test.setName("Test" + testSuiteName);
+        List<XmlClass> classes = new ArrayList<>();
+        classes.add(new XmlClass("test.Test" + testSuiteName));
+        test.setXmlClasses(classes);
+        return suite;
     }
 }
