9f463c7d91e2832c10aca1bdd2647cfc98a60975
Dmitry Danilov <dmitry_danilov@epam.com>
2021-03-04 12:48:08 +0300

    run test by reflection (singleton guice) - code refactoring 2

--- src/test/java/test/B.java
+++ src/test/java/test/B.java
@@ -3,12 +3,13 @@ package test;
 import model.Counter;
 import model.CounterSingleton;
 import test.base.BaseTest;
+import test.base.CommonCondition;
 
-public class B implements BaseTest {
+public class B extends CommonCondition implements BaseTest {
 
     public void method1(String param1) {
 
-        Counter counter = ReflectionTestRunner.injector.getInstance(Counter.class);
+        counter = injector.getInstance(Counter.class);
 
         System.out.println(getClass().getName() + ", method1," + param1);
         System.out.println("Classic Singleton: number = " + CounterSingleton.getInstance().getNumber());
@@ -18,7 +19,7 @@ public class B implements BaseTest {
 
     public void method2(String param1, String param2) {
 
-        Counter counter = ReflectionTestRunner.injector.getInstance(Counter.class);
+        counter = injector.getInstance(Counter.class);
 
         System.out.println(getClass().getName() + ", method2," + param1 + ";" + param2);
         System.out.println("Classic Singleton: number = " + CounterSingleton.getInstance().getNumber());
--- src/test/java/test/ReflectionTestRunner.java
+++ src/test/java/test/ReflectionTestRunner.java
@@ -5,7 +5,6 @@ import com.google.inject.Injector;
 import module.ParentModule;
 import service.FileExtensionNameSelector;
 import service.ParameterBuilderForReflection;
-import service.TestClassProvider;
 import test.base.BaseTest;
 
 import java.lang.reflect.InvocationTargetException;
@@ -15,23 +14,26 @@ public class ReflectionTestRunner {
 
     private static final String filePath = "src/test/resources";
     private static final String fileName = "testdata.txt";
-    public static Injector injector = Guice.createInjector(new ParentModule());
 
     public static void main(String[] args) {
+
+        Injector injector = Guice.createInjector(new ParentModule());
 
         FileExtensionNameSelector.getFileReader(fileName).readData(filePath, fileName).forEach(data -> {
             System.out.println("------------------------");
             System.out.println("TestSuite" + data.getTestClassName());
 
-            BaseTest testObject = TestClassProvider.TestClassByName(data.getTestClassName());
-
-            Class testClass = testObject.getClass();
-            Class[] paramTypes = ParameterBuilderForReflection.createParamsTypes(data.getMethodParameters());
-
             try {
+
+                Class<?> testClass = Class.forName("test." + data.getTestClassName());
+                BaseTest testObject = (BaseTest) injector.getInstance(testClass);
+
+                Class<?>[] paramTypes = ParameterBuilderForReflection.createParamsTypes(data.getMethodParameters());
+
                 Method method = testClass.getMethod(data.getMethodName(), paramTypes);
-                method.invoke(testObject, data.getMethodParameters());
-            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                method.invoke(testObject, (Object[]) data.getMethodParameters());
+
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
         });
